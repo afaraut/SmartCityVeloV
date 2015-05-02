aaf40ee125145a14c2bb93291b7b4005370811d2
Okba <okba.khenissi@insa-lyon.fr>
2015-05-02 20:10:08 +0200

    un peu de css, travail sur l'infowindow

--- getYourBike/search/forms.py
+++ getYourBike/search/forms.py
@@ -5,11 +5,12 @@ from django.forms import extras
 from search.models import SelectTimeWidget
 import csv
 class SearchForm(forms.Form):
-    day = forms.DateField(widget=extras.SelectDateWidget, label="Veuillez choisir votre jour ")
-    hour = forms.TimeField(widget=SelectTimeWidget, label="Veuillez choisir votre heure ")
-    station = forms.CharField(widget=forms.Select, label=u'Veuillez choisir votre station ')
+    station = forms.CharField(widget=forms.Select, label=u'Station')
+    day = forms.DateField(widget=extras.SelectDateWidget, label="Jour")
+    hour = forms.TimeField(widget=SelectTimeWidget, label="Heure ")
+    
 
     def __init__(self, *args, **kwargs):
     	super(SearchForm, self).__init__(*args, **kwargs)
         cr = csv.reader(open('static/longLat.csv',"rb"))
-        self.fields['station'] = forms.CharField(widget=forms.Select(choices=[(raw[1], str(raw[2] + " -- " + raw[1])) for raw in cr]), label=u'Veuillez choisir votre station ')
+        self.fields['station'] = forms.CharField(widget=forms.Select(choices=[(raw[1], str(raw[2] + " -- " + raw[1])) for raw in cr]), label=u'Station ')
