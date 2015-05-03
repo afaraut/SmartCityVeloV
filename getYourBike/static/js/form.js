67d51e2d02d0f6a1c675336d5b5bdb1b12408564
Anthony FARAUT <anthony.faraut@gmail.com>
2015-05-04 00:00:34 +0200

    ajax call

--- getYourBike/static/js/form.js
+++ getYourBike/static/js/form.js
@@ -1,6 +1,8 @@
 //$('#formSearch').live('submit', function(event) { // catch the form's submit event
-$(document).on('submit', '#formSearch', function(){
+//$(document).on('submit', '#formSearch', function(){
+$('#formSearch').submit(function(evt){ 
   event.preventDefault();
+  console.log($(this));
   $('#imgWait').show(); // Show the loading image
   $('body').append('<div id="fade"></div>'); // Add the fade layer to bottom of the body tag.
   $('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); // Fade in the fade layer 
