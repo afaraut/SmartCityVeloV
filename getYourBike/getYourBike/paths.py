63a67502ba250faa48667b8db6c0e6c271e7d1d7
mgarchery <mathieu.garchery@insa-lyon.fr>
2015-05-04 10:11:54 +0200

    added paths.py, added previsionEvaluation.save

---
+++ getYourBike/getYourBike/paths.py
@@ -0,0 +1,7 @@
+
+data_path = '../../data'
+
+db_path = data_path + '/velos'
+vacation_data_path = data_path + '/jours_feries_timestamp.txt'
+regression_path = data_path + '/regression'
+common_path = regression_path + '/common'
--- getYourBike/getYourBike/previsionExample.py
+++ getYourBike/getYourBike/previsionExample.py
@@ -1,13 +1,20 @@
-import prevision
-import util
 import datetime
+import time
 
+
+import prevision
+import previsionEvaluation
+import util
 
 # station
 stationId = 1001
 
 # date/time of prevision ()
 t_prevision = util.datetimeToTimestamp(datetime.datetime(2015,05,10,15)) #GMT time
+t_request = int(time.time())
 
 prev = prevision.previsions(t_prevision, stationId)
 prevision.displayPrevisions(prev, stationId, t_prevision)
+
+#save prevision in DB for future evaluation
+previsionEvaluation.savePrevision(prev, stationId, t_request, t_prevision)
