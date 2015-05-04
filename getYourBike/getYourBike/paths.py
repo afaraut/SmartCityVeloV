c42ab382824fffb5ea0cd54333b0fc8a86a5f38c
Anthony FARAUT <anthony.faraut@gmail.com>
2015-05-04 16:17:20 +0200

    merge

--- getYourBike/getYourBike/paths.py
+++ getYourBike/getYourBike/paths.py
@@ -1,7 +1,12 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8	 -*-
 
-data_path = '../../data'
+import posixpath
 
-db_path = data_path + '/velos'
-vacation_data_path = data_path + '/jours_feries_timestamp.txt'
-regression_path = data_path + '/regression'
-common_path = regression_path + '/common'
+data_path =  posixpath.abspath('../../../data')
+
+
+db_path = data_path + "/velos"
+vacation_data_path = data_path  + '/jours_feries_timestamp.txt'
+regression_path = data_path  + '/regression'
+common_path = regression_path  + '/common'
