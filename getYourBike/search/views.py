2b208e416ef81864398e6549d745aebf19f3e7e0
Anthony FARAUT <anthony.faraut@gmail.com>
2015-05-05 09:58:22 +0200

    web service ok

--- getYourBike/search/views.py
+++ getYourBike/search/views.py
@@ -27,7 +27,7 @@ def test(request):
 @api_view(['GET'])
 def prevision(request, idstation, timestamp):
     prev = previsions(int(timestamp), int(idstation))
-    content = {'prevision': "prev"} # Element for the view
+    content = {'prevision': prev} # Element for the view
     return Response(content) # Return the response
     
 @csrf_exempt
