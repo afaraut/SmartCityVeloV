39a59deea78841bff6a5e06675effea4450805cb
Anthony FARAUT <anthony.faraut@gmail.com>
2015-05-06 10:02:51 +0200

    hour

--- getYourBike/search/forms.py
+++ getYourBike/search/forms.py
@@ -8,10 +8,11 @@ from datetime import date, datetime, time, timedelta
 
 class SearchForm(forms.Form):
     day = forms.DateField(widget=extras.SelectDateWidget, initial=date.today(), label="Jour")
-    hour = forms.DateTimeField(widget=SelectTimeWidget(minute_step=5), initial=time(time().hour+2, time().minute, time().second, time().microsecond), label="Heure ")
+    hour = forms.DateTimeField(widget=SelectTimeWidget(minute_step=5), initial=time(datetime.now().time().hour, datetime.now().time().minute, datetime.now().time().second, datetime.now().time().microsecond), label="Heure ")
     station = forms.CharField(widget=forms.Select, label=u'Station')
 
     def __init__(self, *args, **kwargs):
     	super(SearchForm, self).__init__(*args, **kwargs)
         stations = Station.objects.all()
         self.fields['station'] = forms.CharField(widget=forms.Select(choices=[(stationn.stationNum,  (stationn.stationRegion).encode('utf8') + str(" -- " ) + (stationn.stationName).encode('utf8')) for stationn in stations]), label="Station")
+
